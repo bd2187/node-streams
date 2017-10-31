7d0ca4335aa9e20cddd083297d6d7b767b04328c
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-10-30 18:01:00 -0700

    Utilize readFile method to read greet.txt asynchronously

--- app.js
+++ app.js
@@ -3,4 +3,10 @@ var fs = require("fs");
 // Reads file synchronously
 var greet = fs.readFileSync(`${__dirname}/greet.txt`, "utf8");
 
-console.log(greet);
+// Reads file asyncrhonously
+var greetAsync = fs.readFile(`${__dirname}/greet.txt`, (err, data) => {
+  if (err) throw err;
+  console.log(`Asynchronous: ${data}`);
+});
+
+console.log(`Synchronous: ${greet}`);
